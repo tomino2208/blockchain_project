edef919cdb5baf3ce56e9b6c063cf6e8634866ac
Tom <tomazoulay@campus.technion.ac.il>
2022-03-24 13:16:50 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import pandas as pd
+
+
+def stats_accounts():
+    df = pd.read_csv('merged_trxes_1112trx.csv')
+    num = len(df.index)
+    senders_transacations = df['from_address'].unique()
+    receivers_transacations = df['to_address'].unique()
+    print(f'The total number of transactions analyzed is {num}.\n'
+          f'We found a total of different {senders_transacations.size} senders'
+          f' and a total of {receivers_transacations.size} different receivers.\n'
+          f'The number of transactions in our model would be {senders_transacations.size+receivers_transacations.size}.')
+    return
+
+
+
+if __name__ == '__main__':
+    stats_accounts()
+
